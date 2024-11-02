bb9aeb043c236e581acf0a05bdb84ccaeb7a2fe9
celtzop <adamlord021@gmail.com>
2024-11-02 23:09:53 +0000

    lallaa

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,33 @@
 // components/Header.js
+import { useSession } from 'next-auth/react'; // Import useSession to access session data
 import Link from 'next/link';
 import styles from '../styles/Header.module.css';
 
 const Header = () => {
+  const { data: session } = useSession(); // Get session data
+
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
         <img src="/apollo_logo.png" alt="Apollo Logo" className={styles.logo} />
         <h1 className={styles.title}>APOLLO</h1>
       </div>
-      <nav className={styles.nav}>
-        <Link href="/" className={styles.navItem}>Dashboard</Link>
-        <Link href="/reports" className={styles.navItem}>Reports</Link>
-        <Link href="/settings" className={styles.navItem}>Settings</Link>
-        <Link href="/logout" className={styles.navItem}>Logout</Link>
-      </nav>
-      <div className={styles.securityWarning}>Top Secret Access - Authorized Personnel Only</div>
+      <div className={styles.rightContainer}> {/* New container for nav and warning */}
+        <nav className={styles.nav}>
+          <Link href="/dashboard" className={styles.navItem}>Dashboard</Link>
+          <Link href="/reports" className={styles.navItem}>Reports</Link>
+        </nav>
+        <div className={styles.securityWarning}> Authorized Personnel Only</div>
+        {session && session.user && session.user.image && ( // Check if session and user exist
+          <Link href="/settings" className={styles.profileLink}>
+            <img 
+              src={session.user.image} // Use the session user's image
+              alt="User Profile" 
+              className={styles.profilePicture} 
+            />
+          </Link>
+        )}
+      </div>
     </header>
   );
 };
