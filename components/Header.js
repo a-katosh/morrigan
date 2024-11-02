15daaff3b3a911de513d8c79dc7b74810b711e97
celtzop <adamlord021@gmail.com>
2024-11-02 22:08:34 +0000

    AKWATS VE TREEEE

--- components/Header.js
+++ components/Header.js
@@ -1,41 +1,23 @@
-import { useSession, signOut } from "next-auth/react";
-import { useState } from "react";
-import styles from "../styles/Header.module.css";
+// components/Header.js
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
 
-export default function Header() {
-  const { data: session } = useSession();
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
-
+const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
         <img src="/apollo_logo.png" alt="Apollo Logo" className={styles.logo} />
         <h1 className={styles.title}>APOLLO</h1>
       </div>
-
-      <div className={styles.profileContainer}>
-        {session && session.user.image && (
-          <img
-            src={session.user.image}
-            alt="User Avatar"
-            className={styles.avatar}
-            onClick={toggleDropdown}
-          />
-        )}
-        <div className={`${styles.dropdown} ${dropdownOpen ? styles.show : ""}`}>
-          <button onClick={toggleDropdown} className={styles.dropdownToggle}>
-            Options
-          </button>
-          <div className={styles.dropdownMenu}>
-            <button className={styles.dropdownItem}>Settings</button>
-            <button className={styles.dropdownItem} onClick={() => signOut()}>
-              Logout
-            </button>
-          </div>
-        </div>
-      </div>
+      <nav className={styles.nav}>
+        <Link href="/" className={styles.navItem}>Dashboard</Link>
+        <Link href="/reports" className={styles.navItem}>Reports</Link>
+        <Link href="/settings" className={styles.navItem}>Settings</Link>
+        <Link href="/logout" className={styles.navItem}>Logout</Link>
+      </nav>
+      <div className={styles.securityWarning}>Top Secret Access - Authorized Personnel Only</div>
     </header>
   );
-}
+};
+
+export default Header;
