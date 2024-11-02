a05a96c22a9eeede0cb806cd850dce1fc965bc70
celtzop <adamlord021@gmail.com>
2024-11-02 20:54:56 +0000

    redesignm

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,22 @@ import styles from "../styles/SignIn.module.css";
 export default function SignInPage() {
   return (
     <div className={styles.container}>
+      <div className={styles.overlay}></div>
+      
       <div className={styles.card}>
-        {/* Logo in the middle */}
+        {/* Logo in the center */}
         <img src="/apollo_logo.png" alt="Apollo Logo" className={styles.logo} />
-        
-        <h1 className={styles.title}>Welcome to Apollo</h1>
-        <p className={styles.subtitle}>Authorized Personnel Only</p>
+
+        <h1 className={styles.title}>APOLLO OPERATIONS</h1>
+        <p className={styles.subtitle}>National Security Portal Access</p>
         
         <button onClick={() => signIn("discord")} className={styles.signInButton}>
           Sign in with Discord
         </button>
+        
+        <p className={styles.footerText}>
+          Restricted Access - Authorized Personnel Only
+        </p>
       </div>
     </div>
   );
