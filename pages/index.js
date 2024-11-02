95a416b3f97cc2fc4f0d2e45b1520f825822a2d4
celtzop <adamlord021@gmail.com>
2024-11-02 21:08:50 +0000

    ate?

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,27 @@ export default function SignInPage() {
   return (
     <div className={styles.container}>
       <div className={styles.overlay}></div>
-      
+
       <div className={styles.card}>
+        {/* Top Secret Banner */}
+        <div className={styles.banner}>TOP SECRET</div>
+
         {/* Logo in the center */}
         <img src="/apollo_logo.png" alt="Apollo Logo" className={styles.logo} />
 
         <h1 className={styles.title}>APOLLO</h1>
         <p className={styles.subtitle}>National Security Agency</p>
+
+        {/* Animated restricted access indicator */}
+        <div className={styles.restrictedIndicator}>Restricted Access - Authorized Personnel Only</div>
         
         <button onClick={() => signIn("discord")} className={styles.signInButton}>
           Authenticate
         </button>
-        
+
+        {/* Footer Reminder */}
         <p className={styles.footerText}>
-          Restricted Access - Authorized Personnel Only
+          Unauthorized Access Will Be Monitored and Logged
         </p>
       </div>
     </div>
