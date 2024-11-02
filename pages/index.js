6adcaf6a0e27b5e72ba43604ee2a8d60b59d94c4
celtzop <adamlord021@gmail.com>
2024-11-02 21:23:31 +0000

    DID I EAT OR WHAT GIRL

--- pages/index.js
+++ pages/index.js
@@ -5,28 +5,30 @@ import styles from "../styles/SignIn.module.css";
 export default function SignInPage() {
   return (
     <div className={styles.container}>
+      {/* Background matrix overlay */}
+      <div className={styles.matrix}></div>
       <div className={styles.overlay}></div>
 
       <div className={styles.card}>
         {/* Top Secret Banner */}
-        <div className={styles.banner}>TOP SECRET</div>
+        <div className={styles.banner}>TOP SECRET // AUTHORIZED ACCESS ONLY</div>
 
-        {/* Logo in the center */}
+        {/* Logo and title */}
         <img src="/apollo_logo.png" alt="Apollo Logo" className={styles.logo} />
-
         <h1 className={styles.title}>APOLLO</h1>
-        <p className={styles.subtitle}>National Security Agency</p>
+        <p className={styles.subtitle}>National Security Agency // Secure Login</p>
 
-        {/* Animated restricted access indicator */}
-        <div className={styles.restrictedIndicator}>Restricted Access - Authorized Personnel Only</div>
-        
+        {/* Warning text that appears above the button */}
+        <div className={styles.warningText}>* Unauthorized access is prohibited by Federal Law *</div>
+
+        {/* Button for signing in */}
         <button onClick={() => signIn("discord")} className={styles.signInButton}>
-          Authenticate
+          Begin Authentication
         </button>
 
-        {/* Footer Reminder */}
+        {/* Footer warning */}
         <p className={styles.footerText}>
-          Unauthorized Access Will Be Monitored and Logged
+          SYSTEM MONITORING ACTIVE. ALL ACCESS IS LOGGED AND TRACKED.
         </p>
       </div>
     </div>
